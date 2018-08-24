e62be87a4e458009e15a5d2dbb70f19cb49a3b82
Łukasz Grzybowski-Glikman <kowgli@kowgli.pl>
2018-08-24 11:27:11 +0200

    Added backuper tests

---
+++ ManagedSolutionSitemapFixer.Tests/BackuperTests.cs
@@ -0,0 +1,27 @@
+﻿using ManagedSolutionSitemapFixer.Helpers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManagedSolutionSitemapFixer.Tests
+{
+    [TestClass]
+    public class BackuperTests
+    {
+        [TestMethod]
+        public void Can_PerformTheBackup()
+        {   
+            string backupPath = Backuper.Backup(@"TestFiles\text_file.txt");
+
+            string read = File.ReadAllText(backupPath);
+
+            File.Delete(backupPath);
+
+            Assert.AreEqual("hello", read);
+        }
+    }
+}
